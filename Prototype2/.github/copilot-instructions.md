f5a2104791e179632cf99d02859b0e0382cc5f03
Natálie Teplická <teplicka.natalie@gmail.com>
2025-08-05 00:40:43 +0100

    Finished Challenge 2

--- Prototype2/.github/copilot-instructions.md
+++ Prototype2/.github/copilot-instructions.md
@@ -1,8 +1,4 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: Prototype2
 - Unity version: Unity 6000.1.10f1
-- Active game object:
-  - Name: Food_Cookie_01
-  - Tag: Untagged
-  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Check if the player has reached the limit of dogs
+            if (GameObject.FindGameObjectsWithTag("Dog").Length == 1)
+            {
+                Debug.Log("You have reached the limit of dogs!");
+                return;
+            }
+            else
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+        }
+    }
+}
